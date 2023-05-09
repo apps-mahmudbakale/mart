808efc243646608708db0de72d8b81e6d85c40c9
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2023-05-09 20:33:04 +0100

    WIP

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-50">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/png">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+</head>
+<body class="h-full">
+   @yield('content')
+</body>
+</html>
